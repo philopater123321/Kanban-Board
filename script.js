61573ef1b2b8f8d686385b10969e57f1dd536d61
philopater123321 <philopater.2572009@example.com>
2026-06-20 22:18:12 +0300

    wrote the delete button structure code in JavaScript

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const taskInput = document.getElementById('task-input');
 const addBtn = document.getElementById('add-task-button');
 const todoColumn = document.getElementById('todo');
+const container = document.querySelector('.container');
 
 addBtn.addEventListener('click', function() {
 
     if (taskInput.value.trim() !== '') {
         const newTask = document.createElement('div');
         newTask.classList.add('task-card');
-        newTask.textContent = taskInput.value;
+
+        newTask.innerHTML = `${taskInput.value} <span class="delete-btn">✖</span>`;
+
         todoColumn.appendChild(newTask);
         taskInput.value= '';
     } else{
         alert('Please enter a task before adding...');
+    }
+});
+
+container.addEventListener('click', function(event) {
+    if (event.target.classList.contains('delete-btn')) {
+        event.target.parentElement.remove();
     }
 });
